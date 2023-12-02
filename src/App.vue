9dc59f0f5f65521eba8e10a4213c9f6a5aac02f2
Olka1516 <olkakorolcuk@gmail.com>
2023-12-02 17:25:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Header from './views/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
+  <RouterView />
+</template>
---
+++ src/views/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const test = () => {
+  console.log('hello')
+}
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <div class="content">
+        <img class="logo" src="" alt="Logo" />
+      </div>
+      <button class="account-button" @click="test">Sign</button>
+    </header>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/scss/index';
+@include index.header;
+</style>
