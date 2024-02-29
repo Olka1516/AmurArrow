47f6ba805a133f0817b9147ddffce3fcafaadb5b
Olka1516 <olkakorolcuk@gmail.com>
2024-02-29 02:07:58 +0200

    Create form for posts add page with image, title and description

--- src/views/UserAddPosts.vue
+++ src/views/UserAddPosts.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+import DragFile from '@/components/general/DragFile.vue'
+import TextInput from '@/components/general/TextInput.vue'
+import DescriptionInput from '@/components/general/DescriptionInput.vue'
+import { ref } from 'vue'
+
+const description = ref('')
+const title = ref('')
+</script>
 <template>
-    <h1>hello</h1>
+  <section>
+    <div v-for="n in 261" :key="n">
+      <span></span>
+    </div>
+
+    <div class="user-posts">
+      <div class="user-posts-content">
+        <DragFile :isPostsPage="true" />
+        <hr />
+        <div class="user-content-inputs">
+          <div class="form-input">
+            <TextInput v-model="title" type="Title" />
+          </div>
+          <div class="form-input">
+            <DescriptionInput v-model="description" type="Description" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+<style scoped lang="scss">
+@use '@/assets/scss/index';
+@include index.user-posts;
+</style>
